b863ab6445cdf8abdbb1956546177aa469494905
Matthew McDermott <mattmcdermott8@gmail.com>
2025-04-11 14:35:12 -0400

    Added initial files.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# MEDS_EIC_AR
+# MEDS "Everything-is-code" Autoregressive Model
 A MEDS, "Everything-is-code" style Autoregressive Generative Model, capable of zero-shot inference.
+
+This is based on the [MEDS-Torch](https://github.com/Oufattole/meds-torch) model of the same name.
+
+## Installation
+
+```bash
+pip install MEDS-EIC-AR
+```
+
+## Differences from the MEDS-Torch version
+
+  1. In comparison to the MEDS-Torch
+     [config](https://github.com/Oufattole/meds-torch/blob/d1650ea6152301a9b9bdbd32756337214e5f310f/ZERO_SHOT_TUTORIAL/configs/eic_config.yaml),
+     this version removes the "reorder measurements" pre-processing step and the additional code filtering
+     that is specific to MIMIC.
+  2. The
+     [`custom_time_token.py`](https://github.com/Oufattole/meds-torch/blob/d1650ea6152301a9b9bdbd32756337214e5f310f/src/meds_torch/utils/custom_time_token.py)
+     script has been renamed to
+     [`add_time_interval_tokens.py`](src/MEDS_EIC_AR/stages/add_time_interval_tokens.py) and exported as a
+     [script](pyproject.toml) in this package.
+  3. The `custom_filter_measurements` script is replaced with the generic filter measurements augmented with
+     the match and revise syntax.
+  4. The
+     [`quantile_binning.py`](https://github.com/Oufattole/meds-torch/blob/d1650ea6152301a9b9bdbd32756337214e5f310f/src/meds_torch/utils/quantile_binning.py)
+     script has been copied to
+     [`quantile_binning.py`](src/MEDS_EIC_AR/stages/quantile_binning.py) and exported as a
+     [script](pyproject.toml) in this package.
+  5. The list of included stages has been revised to include only the ones that are uniquely relevant to this
+     model. Data tokenization and tensorization stages have been outsourced to
+     [meds-torch-data](https://meds-torch-data.readthedocs.io/en/latest/)
+
---
+++ src/MEDS_EIC_AR/__main__.py
@@ -0,0 +1,70 @@
+from importlib.resources import files
+import hydra
+from omegaconf import DictConfig
+from pathlib import Path
+import subprocess
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+CONFIGS = files(__package__).joinpath("configs")
+
+@hydra.main(version_base=None, config_path=str(CONFIGS), config_name="_process_data")
+def process_data(cfg: DictConfig):
+    input_dir = Path(cfg.input_dir)
+    intermediate_dir = Path(cfg.intermediate_dir)
+    output_dir = Path(cfg.output_dir)
+
+    # 0. Pre-MTD pre-processing
+    logger.info("Pre-MTD pre-processing")
+    done_fp = intermediate_dir / ".done"
+    if done_fp.exists():
+        logger.info("Pre-MTD pre-processing already done, skipping")
+    else:
+        env = copy.deepcopy(os.environ)
+        env["RAW_MEDS_DIR"] = str(input_dir)
+        env["MTD_INPUT_DIR"] = str(intermediate_dir)
+
+        pipeline_config_fp = CONFIGS / "_data.yaml"
+        cmd = [
+            "MEDS_transform-runner",
+            f"pipeline_config_fp={pipeline_config_fp!s}",
+        ]
+        logger.info(f"Running command: {' '.join(cmd)}")
+
+        result = subprocess.run(cmd, env=env, capture_output=True, check=False)
+        if result.returncode != 0:
+            logger.error("Error running MEDS_transform-runner")
+            logger.error(result.stderr.decode())
+            raise RuntimeError("Error running MEDS_transform-runner")
+
+        logger.info("Pre-MTD pre-processing done")
+        done_fp.touch()
+
+    # 1. Run MTD pre-processing
+    logger.info("Running MTD pre-processing")
+    done_fp = output_dir / ".done"
+
+    if done_fp.exists():
+        logger.info("MTD pre-processing already done, skipping")
+    else:
+        env = copy.deepcopy(os.environ)
+
+        cmd = [
+            "MTD_preprocess",
+            f"input_dir={intermediate_dir!s}",
+            f"output_dir={output_dir!s}",
+        ]
+        logger.info(f"Running command: {' '.join(cmd)}")
+
+        result = subprocess.run(cmd, env=env, capture_output=True, check=False)
+        if result.returncode != 0:
+            logger.error("Error running MTD_preprocess")
+            logger.error(result.stderr.decode())
+            raise RuntimeError("Error running MTD_preprocess")
+
+        logger.info("MTD pre-processing done")
+        done_fp.touch()
+
+    return
