3d6f3d75afb88af71e1da1c14772df2fb52ae3ca
Matthew McDermott <mattmcdermott8@gmail.com>
2025-04-13 11:31:19 -0400

    Further work on configs

--- src/MEDS_EIC_AR/__main__.py
+++ src/MEDS_EIC_AR/__main__.py
@@ -7,20 +7,23 @@ from hydra.core.config_store import ConfigStore
 from hydra.utils import instantiate
 from meds import code_metadata_filepath
 from meds_torchdata import MEDSTorchDataConfig
-from omegaconf import DictConfig
+from omegaconf import DictConfig, open_dict
 
 logger = logging.getLogger(__name__)
 
 CONFIGS = files("MEDS_EIC_AR") / "configs"
 
 cs = ConfigStore.instance()
-cs.store(name="MEDSTorchDataConfig", node=MEDSTorchDataConfig)
+cs.store(group="dataset/cfg", name="MEDSTorchDataConfig", node=MEDSTorchDataConfig)
+cfg_node = cs.repo["dataset"]["cfg"]["MEDSTorchDataConfig.yaml"].node
+with open_dict(cfg_node):
+    cfg_node["_target_"] = "meds_torchdata.MEDSTorchDataConfig"
 
 
 @hydra.main(version_base=None, config_path=str(CONFIGS), config_name="_train")
 def train(cfg: DictConfig):
     D = instantiate(cfg.dataset)
-    metadata_df = pl.read_parquet(D.cfg.tensorized_cohort_dir / code_metadata_filepath, use_pyarrow=True)
+    metadata_df = pl.read_parquet(D.config.tensorized_cohort_dir / code_metadata_filepath, use_pyarrow=True)
     D.vocab_size = metadata_df.select(pl.col("code/vocab_index")).max().item() + 1
 
     M = instantiate(cfg.lightning_module, vocab_size=D.vocab_size)
