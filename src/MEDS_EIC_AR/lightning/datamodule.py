06436373ac955fe62ff07b2385f6dbdb6c080409
Matthew McDermott <mattmcdermott8@gmail.com>
2025-04-17 15:44:55 -0400

    Removed unstable train batch test -- seeding alone was insufficient

--- src/MEDS_EIC_AR/lightning/datamodule.py
+++ src/MEDS_EIC_AR/lightning/datamodule.py
@@ -21,49 +21,18 @@ class Datamodule(L.LightningDataModule):
     After construction, we can access dataloaders for training, validation, and testing:
 
         >>> val_dataloader = D.val_dataloader()
-        >>> val_batch = next(iter(val_dataloader))
-        >>> val_batch
-        MEDSTorchBatch(code=tensor([[37,  5, 32,  3, 14, 28, 36,  4]]),
-                       numeric_value=tensor([[ 0.0000,  0.0000, -6.7254,  0.0000,
-                                               0.0000, -1.1314,  0.2792,  0.0000]]),
-                       numeric_value_mask=tensor([[False, False,  True, False,
-                                                   False,  True,  True, False]]),
-                       time_delta_days=tensor([[0.0000e+00, 0.0000e+00, 7.6853e+03, 0.0000e+00,
-                                                0.0000e+00, 0.0000e+00, 7.9329e-02, 0.0000e+00]]),
-                       event_mask=None,
-                       static_code=tensor([[ 7, 12]]),
-                       static_numeric_value=tensor([[ 0.0000e+00, -3.4028e+38]]),
-                       static_numeric_value_mask=tensor([[False,  True]]),
-                       boolean_value=None)
-
-    By default, the train dataloader shuffles, which is suitable for training but less so for doctest outputs.
-    We can account for this by seeding, however:
-
-        >>> _ = L.seed_everything(0)
-        >>> train_dataloader = D.train_dataloader()
-        >>> train_batch = next(iter(train_dataloader))
-        >>> train_batch
-        MEDSTorchBatch(code=tensor([[37,  5, 32,  1, 13, 25, 33, 15, 26, 35],
-                                    [37,  5, 32,  2, 14, 30, 36,  4,  0,  0]]),
-                       numeric_value=tensor([[ 0.0000,  0.0000, -1.5495,  0.0000,  1.0000,
-                                               0.0000, -1.0000,  0.0000, 0.0000,  1.0000],
-                                             [ 0.0000,  0.0000,  1.2423,  0.0000,  0.0000,
-                                               0.0000, -1.0000,  0.0000, 0.0000,  0.0000]]),
+        >>> next(iter(val_dataloader))
+        MEDSTorchBatch(code=tensor([[38,  5, 36,  3, 22, 29, 35,  4, 37]]),
+                       numeric_value=tensor([[ 0.0000,  0.0000, -6.7254,  0.0000, -0.1190,
+                                               0.9995,  0.2792,  0.0000, 0.0000]]),
                        numeric_value_mask=tensor([[False, False,  True, False,  True,
-                                                   False,  True, False, False,  True],
-                                                  [False, False,  True, False, False,
-                                                   False,  True, False,  True,  True]]),
-                       time_delta_days=tensor([[0.0000e+00, 0.0000e+00, 1.0727e+04, 0.0000e+00, 0.0000e+00,
-                                                0.0000e+00, 4.8264e-03, 0.0000e+00, 0.0000e+00, 2.5544e-02],
-                                               [0.0000e+00, 0.0000e+00, 1.2367e+04, 0.0000e+00, 0.0000e+00,
-                                                0.0000e+00, 4.6424e-02, 0.0000e+00, 0.0000e+00, 0.0000e+00]]),
+                                                   True,  True, False, False]]),
+                       time_delta_days=tensor([[0.0000e+00, 0.0000e+00, 7.6853e+03, 0.0000e+00, 0.0000e+00,
+                                                0.0000e+00, 7.9329e-02, 0.0000e+00, 0.0000e+00]]),
                        event_mask=None,
-                       static_code=tensor([[ 7, 12],
-                                           [ 8,  9]]),
-                       static_numeric_value=tensor([[0., 0.],
-                                                    [0., 0.]]),
-                       static_numeric_value_mask=tensor([[False, False],
-                                                         [False, False]]),
+                       static_code=tensor([[ 7, 11]]),
+                       static_numeric_value=tensor([[0.0000e+00, 3.4028e+38]]),
+                       static_numeric_value_mask=tensor([[False,  True]]),
                        boolean_value=None)
     """
 
