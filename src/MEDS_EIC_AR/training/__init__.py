a7b505ac25406656cd729f8099fa8986d47affb9
Matthew McDermott <mattmcdermott8@gmail.com>
2025-06-05 21:52:43 +0800

    Fix logger helper comments and add tests

--- src/MEDS_EIC_AR/training/__init__.py
+++ src/MEDS_EIC_AR/training/__init__.py
@@ -1,3 +1,4 @@
+from .callbacks import GenerationSpeedLogger
 from .files import find_checkpoint_path, validate_resume_directory
 from .metrics import NextCodeMetrics
 from .module import MEICARModule
